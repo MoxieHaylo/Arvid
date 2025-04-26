9452a99789ceb34f4ac79b119ae19168d3b20ec3
Hayley <40880200+MoxieHaylo@users.noreply.github.com>
2025-04-26 16:49:42 +1000

    Torches now go out after being lit

--- Assets/Scripts/Torch.cs
+++ Assets/Scripts/Torch.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Torch : MonoBehaviour, IInteractable
@@ -17,10 +18,18 @@ public class Torch : MonoBehaviour, IInteractable
         isLit=true;
         Debug.Log("Torch lit");
         gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        StartCoroutine(TorchDecay());
         //add visuals
         //start timer with progress bar
         //snuff out
         //mark !isLit
         //gameObject.transform.GetChild(0).gameObject.SetActive(false);
+    }
+    IEnumerator TorchDecay()
+    {
+        yield return new WaitForSeconds(5);
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        Debug.Log("torch went out");
+        isLit = false;
     }
 }
